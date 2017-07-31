b3e8671167410aa9f084518b3ab139ef240e6891
Edson Neri <edson.neri@correo.uia.mx>
2017-07-31 04:17:12 -0500

    add Book model and create function in Book controller

--- application/controllers/Books.php
+++ application/controllers/Books.php
@@ -3,10 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Books extends CI_Controller
 {
-	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('book');
+	}
 	function index()
 	{
 		$this->load->view('books/list');
+	}
+
+	function create()
+	{
+		$new = array(
+			'isbn' => $this->input->post('isbn'),
+			'title' => $this->input->post('title')
+		);
+		$result = $this->book->create($new);
+		if (!$result) {
+			echo json_encode(array('success' => FALSE));
+			return FALSE;
+		}
+		echo json_encode(array('success' => TRUE, 'book' => $result));
+		return TRUE;
 	}
 }
 ?>
---
+++ application/models/Book.php
@@ -0,0 +1,31 @@
+<?php
+class Book extends CI_Model
+{
+	private $table = 'books';
+
+	function create($book)
+	{
+		# code...
+	}
+
+	function retrieve($id)
+	{
+		# code...
+	}
+
+	function retrieveAll()
+	{
+		# code...
+	}
+
+	function update($updated)
+	{
+		# code...
+	}
+
+	function delete($id)
+	{
+		# code...
+	}
+}
+?>
